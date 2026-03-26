d9da0c6e2bb7c501cb899f0bfc54efacca41ca5d
undragonIII <jmgonzalezs@fi.uba.ar>
2026-03-26 12:54:52 -0300

    agrego soporte de batch al protocolo del cliente

--- client/common/protocol.go
+++ client/common/protocol.go
@@ -35,24 +35,20 @@ func sendField(conn net.Conn, field string) error {
 	data := []byte(field)
 	length := uint16(len(data))
 
-	// Envio la longitud del campo (2 bytes, big-endian)
 	lenBuf := make([]byte, 2)
 	binary.BigEndian.PutUint16(lenBuf, length)
 	if err := sendAll(conn, lenBuf); err != nil {
 		return fmt.Errorf("error enviando longitud: %v", err)
 	}
 
-	// Envio los datos del campo
 	if err := sendAll(conn, data); err != nil {
 		return fmt.Errorf("error enviando datos: %v", err)
 	}
 	return nil
 }
 
-// SendBet serializa y envia una apuesta por el socket usando el protocolo custom.
-// Formato: [agencia][nombre][apellido][documento][nacimiento][numero]
-// Cada campo se envia como [2 bytes longitud][datos]
-func SendBet(conn net.Conn, agency string, bet Bet) error {
+// sendBet serializa y envia una apuesta individual por el socket.
+func sendBet(conn net.Conn, agency string, bet Bet) error {
 	fields := []string{
 		agency,
 		bet.FirstName,
@@ -64,6 +60,24 @@ func SendBet(conn net.Conn, agency string, bet Bet) error {
 
 	for _, field := range fields {
 		if err := sendField(conn, field); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SendBetBatch envia un batch de apuestas. Primero envia la cantidad (2 bytes),
+// luego cada apuesta individualmente.
+func SendBetBatch(conn net.Conn, agency string, bets []Bet) error {
+	// Envio la cantidad de apuestas en el batch
+	countBuf := make([]byte, 2)
+	binary.BigEndian.PutUint16(countBuf, uint16(len(bets)))
+	if err := sendAll(conn, countBuf); err != nil {
+		return fmt.Errorf("error enviando cantidad de apuestas: %v", err)
+	}
+
+	for _, bet := range bets {
+		if err := sendBet(conn, agency, bet); err != nil {
 			return err
 		}
 	}
